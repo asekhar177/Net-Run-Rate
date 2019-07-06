4b3dea9379427745a99b68b794d06a0605cc42f4
Arjun Sekhar <51122921+asekhar177@users.noreply.github.com>
2019-07-07 00:53:49 +1000

    Update NetRunRate.R

--- NetRunRate.R
+++ NetRunRate.R
@@ -2,4 +2,4 @@
 tournament_run_rate = function(total_runs_scored_all, total_overs_faced_all, total_runs_conceded_all, total_overs_bowled_all){
   return((total_runs_scored_all/total_overs_faced_all) - (total_runs_conceded_all/total_overs_bowled_all))
 }
-
+#Note that if the team we are calculating bowled another team out, it counts as 50 overs used in that innings.
